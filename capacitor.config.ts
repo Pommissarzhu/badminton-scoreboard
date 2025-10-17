d886b783e8648564a6823fef2ee9a743576c4d6d
Pommissarzhu <zhuzhengwei14@126.com>
2025-10-17 13:48:30 +0800

    feat: Add Capacitor and initial config for iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.badmintonscore',
+  appName: 'Badminton Score',
+  webDir: 'build'
+};
+
+export default config;
